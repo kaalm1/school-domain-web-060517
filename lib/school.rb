384b2f8411a01ac1938fb66bbefae1eb852dd7b6
kaalm1 <kaalm1@gmail.com>
2017-06-08 13:56:31 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
 # code here!
+require 'pry-byebug'
+
+class School
+  attr_accessor :roster
+
+  def initialize(school_name)
+    @roster = {}
+  end
+
+  def add_student(name,grade)
+    @roster[grade] = [] if @roster[grade].nil?
+    @roster[grade] << name
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |grade,students|
+      @roster[grade] = students.sort
+    end
+  end
+end
